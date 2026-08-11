23036aae16910dfd31977d8a347f7d6110eb9d39
lyhottag13 <leonardo@promoevt.com>
2026-08-11 16:35:17 -0700

    Shortened VHS audio and forced fullscreen

--- src/scripts/main_game.gd
+++ src/scripts/main_game.gd
@@ -24,10 +24,15 @@ func _ready() -> void:
 	world.add_child(player)
 	
 	player.global_position = maze.get_spawn()
+	
 	SoundManager.play_background("VHS")
 	
 	maze.area_entered.connect(_on_area_entered)
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _on_area_entered(area_direction: String, new_position: Vector2) -> void:
 	player.position = new_position
